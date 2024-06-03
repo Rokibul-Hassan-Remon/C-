04c1ac7838b29d4d3c5e96d4032a0a4639cacc14
Rokibul-Hassan-Remon <117553114+Rokibul-Hassan-Remon@users.noreply.github.com>
2024-06-03 19:51:59 +0600

    Add files via upload

---
+++ SwitchCase/ConsoleApp2/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please input the student no :\n");
+            Int32 a = Convert.ToInt32(Console.ReadLine());
+           
+            string grade;
+
+            switch (a/10)
+            { 
+              case 10:
+
+              case 9:
+                
+              case 8:
+                    grade = "A+";
+                    break;
+
+              case 7:
+                     grade = "A";
+                     break;
+              case 6:
+                    grade = "A-";
+                    break;
+              case 5:
+                    grade = "B";
+                    break;
+              case 4:
+                    grade = "C";
+                    break;
+              case 3:
+                    grade = "D";
+                    break;
+              default :
+                    grade = "F";
+                    break;
+            }
+
+            Console.WriteLine(grade);
+            Console.ReadLine();
+
+        }
+    }
+}

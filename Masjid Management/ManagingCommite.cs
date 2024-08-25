b09571531f72e9807f4d1da67cc20c5025dc2325
Rokibul-Hassan-Remon <117553114+Rokibul-Hassan-Remon@users.noreply.github.com>
2024-08-25 11:16:00 +0600

    Add files via upload

---
+++ Masjid Management/ManagingCommite.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Masjid_Management
+{
+    public class ManagingCommite : People
+    {
+        public ManagingCommite(string name, string position, int donate) : base(name,position,donate)
+        {
+             
+        }
+
+        public ManagingCommite(string name, string position) : base(name, position, 0)
+        {
+
+        }
+
+    }
+}
---
+++ Masjid Management/Prayer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Masjid_Management
+{
+    internal class Prayer : People
+    {
+        public Prayer(string name, string position, int donate) : base(name, position, donate)
+        {
+            
+        }
+
+        public Prayer(string name, string position) : base(name, position, 0)
+        {
+
+        }
+    }
+}
